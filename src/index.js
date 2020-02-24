ffa06e676898c39debc80d8bf4ec0a66d8ff48cb
BabyDragon003 <golddragon01234@gmail.com>
2020-02-24 11:28:32 +0900

    "2020-02-24 11:28"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const express = require("express");
 const cors = require("cors");
 const bodyParser = require('body-parser');
 const http = require("http");
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,2 @@
-module.exports = {
     url: "mongodb://localhost:27017/terrapexc"
 }
