c7e8e480c5237ebc6a7e4535284bd882f8b55091
BabyDragon003 <golddragon01234@gmail.com>
2020-03-25 11:28:32 +0900

    "2020-03-25 11:28"

--- src/db/index.js
+++ src/db/index.js
@@ -3,3 +3,8 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 const db = {};
+db.mongoose = mongoose;
+db.url = dbConfig.url;
+db.Orders = require("./orders.model")(mongoose);
+
+module.exports = db;
